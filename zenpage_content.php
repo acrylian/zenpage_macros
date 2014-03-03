567f702263b2c5c23e1fe4f5d5f1eb47ce911fdf
acrylian <wartgart@gmx.net>
2014-03-03 19:28:08 +0100

    First parameter should not be optional.

--- zenpage_content.php
+++ zenpage_content.php
@@ -74,28 +74,28 @@ class zenpageContent {
 	static function zenpageContent_macro($macros) {
 		$macros['PAGECONTENT'] = array(
 					'class'=>'function',
-					'params'=> array('string*','bool*'), 
+					'params'=> array('string','bool*'), 
 					'value'=>'zenpageContent::getPageContent',
 					'owner'=>'zenpageContent',
 					'desc'=>gettext('Prints the content of the page with titlelink (%1) being published true|false (%2).')
 				);
 		$macros['PAGEEXTRACONTENT'] = array(
 					'class'=>'function',
-					'params'=> array('string*','bool*'), 
+					'params'=> array('string','bool*'), 
 					'value'=>'zenpageContent::getPageExtraContent',
 					'owner'=>'zenpageContent',
 					'desc'=>gettext('Prints the extra content of the page with titlelink (%1) being published true|false (%2).')
 				);
 		$macros['NEWSCONTENT'] = array(
 					'class'=>'function',
-					'params'=> array('string*','bool*'), 
+					'params'=> array('string','bool*'), 
 					'value'=>'zenpageContent::getArticleContent',
 					'owner'=>'zenpageContent',
 					'desc'=>gettext('Prints the content of the ews article with titlelink (%1) being published true|false (%2).')
 				);
 		$macros['NEWSEXTRACONTENT'] = array(
 					'class'=>'function',
-					'params'=> array('string*','bool*'), 
+					'params'=> array('string','bool*'), 
 					'value'=>'zenpageContent::getArticleExtraContent',
 					'owner'=>'zenpageContent',
 					'desc'=>gettext('Prints the extra content of the news article with titlelink (%1) being published true|false (%2).')
